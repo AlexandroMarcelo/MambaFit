cfebeb58a15e0260741203baca2cbb4c82230e67
AlexandroMarcelo <alex.fcomarcelo@gmail.com>
2019-10-28 00:01:30 -0600

    Mamba Fit finished

---
+++ api/config.py
@@ -0,0 +1,17 @@
+# --- On cloud resources ---
+# Mongo configuration
+#MONGO_URI = 'connectionString'
+MONGO_URI = 'mongodb+srv://Admin:aaa@cluster0-0nw5b.gcp.mongodb.net/test?retryWrites=true'
+# Redis configuration
+#REDIS_HOST = 'connectionString'
+#REDIS_PORT = port
+#REDIS_PASSWORD = 'password'
+
+# --- Localhost resources ---
+# # Mongo configuration
+#MONGO_URI = 'mongodb://localhost:27017/gimnasio'
+#
+# # Redis configuration
+REDIS_HOST = 'redis-14025.c53.west-us.azure.cloud.redislabs.com'
+REDIS_PORT = 14025
+REDIS_PASSWORD = 'Ao0oSS6ZIRVOtgqM6zAIjkSRebaN29y5'
